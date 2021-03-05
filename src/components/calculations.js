260ce5c1d2e63b6339399696680a9175ff49f6e9
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2021-03-05 10:44:35 -0600

    19 - Testing Finished for Calculations

--- src/components/calculations.js
+++ src/components/calculations.js
@@ -1,10 +1,15 @@
 import dayjs from 'dayjs';
 
-export function calculateBiorhythm(birthdate, targetDate) {
+function calculateBiorhythm(birthdate, targetDate, cycle) {
     const birthday = dayjs(birthdate).startOf('day');
     const targetDay = dayjs(targetDate).startOf('day');
     const diff = targetDay.diff(birthday, 'days');
-
-    return Math.sin(2 * Math.PI * diff / 23);
-
+    return Math.sin(2 * Math.PI * diff / cycle);
+}
+export function calculateBiorhythms(birthdate, targetDate) {
+    return {
+        physical: calculateBiorhythm(birthdate, targetDate, 23),
+        emotional: calculateBiorhythm(birthdate, targetDate, 28),
+        intellectual: calculateBiorhythm(birthdate, targetDate, 33)
+    };
 }
--- src/components/calculations.test.js
+++ src/components/calculations.test.js
@@ -1,6 +1,14 @@
-import { calculateBiorhythm } from './calculations';
+import { calculateBiorhythms } from './calculations';
 
-it ('calculates the biorhythm', () => {
-    const result = calculateBiorhythm('1995-01-01', '2020-02-18');
-    expect (result).toBeCloseTo(0.5196);
+it ('calculates the physical biorhythm', () => {
+    const { physical } = calculateBiorhythms('1995-01-01', '2020-02-18');
+    expect (physical).toBeCloseTo(0.5196);
+});
+it ('calculates the emotional biorhythm', () => {
+    const { emotional } = calculateBiorhythms('1995-01-01', '2020-02-18');
+    expect (emotional).toBeCloseTo(-0.9010);
+});
+it ('calculates the intellectual biorhythm', () => {
+    const { intellectual } = calculateBiorhythms('1995-01-01', '2020-02-18');
+    expect (intellectual).toBeCloseTo(0.8146);
 });
